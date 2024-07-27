584801a7ef64c2b3ff168e0707084d9e9e830b08
Doris Zhang <doriszhang912@gmail.com>
2024-07-27 13:34:42 -0400

    first commit

---
+++ app/landing.tsx
@@ -0,0 +1,48 @@
+
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { Link } from 'expo-router';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+
+export default function LandingPage() {
+
+    let [fontsLoaded] = useFonts({
+        Inter_400Regular,
+        Inter_500Medium,
+      });
+
+  return (
+    <View style= {{width: "100%", height: "100%", justifyContent:'center', 
+    alignSelf:'center', alignContent:'center', alignItems:'center'}}>
+      <Text >Welcome </Text>
+      <Link href="/login">
+        <TouchableOpacity style= {styles.button}>
+            <Text style = {styles.buttonText}>Login</Text>
+        </TouchableOpacity >
+      </Link>
+      <Link href="/signup">
+        <TouchableOpacity style={styles.button}>
+            <Text style = {styles.buttonText}>Sign up</Text>
+        </TouchableOpacity>
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor:'#FFC350',
+        height:50,
+        width:300,
+        alignItems:'center',
+        justifyContent:'center',
+        borderRadius: 25,
+        marginTop: 25
+    },
+    buttonText: {
+        fontSize: 20,
+        color:'white',
+        fontWeight: '500',
+        fontFamily:'Inter_500Medium'
+    }
+    
+});
---
+++ app/signup.tsx
@@ -0,0 +1,108 @@
+import { View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native'
+import { useFonts, Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter';
+import { FontAwesome } from '@expo/vector-icons';
+import { Link } from 'expo-router';
+
+export default function SignUp() {
+
+    let [fontsLoaded] = useFonts({
+        Inter_400Regular,
+        Inter_500Medium,
+        
+      });
+
+    return (
+        <View style= {styles.mainContainer}>
+            <View style= {styles.headerContainer}> 
+                <Link href='/landing'>
+                    <FontAwesome name='arrow-left' style= {{color: '#CFCFCF',
+                    marginLeft: 30
+                    }}></FontAwesome>
+                </Link>
+                
+                <View style= {{ width: '100%', alignItems:'center',
+                    marginTop: 30
+                }}>
+                    <Text style = {styles.header}>
+                        Sign up
+                    </Text>
+                </View>
+            </View>
+            <View style= {styles.container}>
+                
+                <TextInput
+                style = {styles.input}
+                placeholder='Name'
+                />
+                <TextInput
+                style = {styles.input}
+                placeholder='Email'
+                />
+                <TextInput
+                style = {styles.input}
+                placeholder='Password'
+                />
+                <Link href='/home'>
+                    <TouchableOpacity style = {styles.button}>
+                        <Text style = {styles.buttonText}>Sign up</Text>
+                    </TouchableOpacity>
+                </Link>
+            </View>
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        width: '100%',
+        height: '100%',
+    },
+    headerContainer: {
+        width: '100%',
+        height: '10%',
+        marginTop: 25,
+    },
+    container: {
+        width: '100%',
+        height: '90%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    header: {
+        fontSize: 25,
+        fontFamily: 'Inter_500Medium'
+    },
+    input: {
+        marginTop: 30,
+        color: '#CFCFCF',
+        borderWidth: 2,
+        fontSize:18,
+        width: 300,
+        height:50,
+        borderRadius: 25,
+        padding: 2,
+        paddingLeft: 15,
+        borderColor: '#CFCFCF',
+        backgroundColor: '#F1F1F1',
+        fontFamily: 'Inter_400Regular',
+        paddingRight: 15,
+        
+
+    },
+    button: {
+        backgroundColor:'#FFC350',
+        height:50,
+        width:300,
+        alignItems:'center',
+        justifyContent:'center',
+        borderRadius: 25,
+        marginTop: 25
+    },
+    buttonText: {
+        fontSize: 20,
+        color:'white',
+        fontWeight: '500',
+        fontFamily:'Inter_500Medium'
+    }
+})
